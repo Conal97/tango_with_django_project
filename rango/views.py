5bdb70b6b50ca112662861e851cd9e5377ef57c0
Conal97 <conalbrosnan@gmail.com>
2021-07-28 18:08:29 +0100

    Completed chapter 4 exercises

--- rango/views.py
+++ rango/views.py
@@ -9,6 +9,6 @@ def index (request):
 
 def about (request):
 
-    html = "Rango says here is the about page." + '<a href="/rango/">Index</a>'
+    context_dict = {'boldmessage': 'This tutorial has been put together by Conal'}
 
-    return HttpResponse(html)
+    return render(request, 'rango/about.html', context=context_dict)
